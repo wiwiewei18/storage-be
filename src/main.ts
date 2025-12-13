22bb44d86b85d3accfc7849c3d7ebe41bdd5260f
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-12-13 10:33:49 +0900

    chore: configure CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { corsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsConfig);
 
   app.useGlobalPipes(
     new ValidationPipe({
