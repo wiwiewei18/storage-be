a6ae8dec88821a96b2e9a9e9d25c5df4c0924fc4
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-12-20 00:35:07 +0900

    feat: add new column to file schema

---
+++ src/infra/database/drizzle/migrations/0005_chunky_talisman.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "files" ADD COLUMN "object_key" text NOT NULL;
--- src/infra/database/drizzle/schemas/file.schema.ts
+++ src/infra/database/drizzle/schemas/file.schema.ts
@@ -1,8 +1,16 @@
-import { varchar, timestamp, bigint, uuid, pgTable } from 'drizzle-orm/pg-core';
+import {
+  varchar,
+  timestamp,
+  bigint,
+  uuid,
+  pgTable,
+  text,
+} from 'drizzle-orm/pg-core';
 
 export const fileTable = pgTable('files', {
   id: uuid('id').primaryKey(),
   fileOwnerId: uuid('file_owner_id').notNull(),
+  objectKey: text('object_key').notNull(),
   name: varchar('name').notNull(),
   size: bigint('size', { mode: 'number' }).notNull(),
   type: varchar('type').notNull(),
